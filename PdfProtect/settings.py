f99a692888f94e7858502b43d30495c59d42c786
Nkhil Taneja <98747035+tanejanikhil3638@users.noreply.github.com>
2023-06-29 10:32:06 +0530

    Update settings.py

--- PdfProtect/settings.py
+++ PdfProtect/settings.py
@@ -138,6 +138,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER ='nikhiltaneja820@gmail.com'
-EMAIL_HOST_PASSWORD ='fljg qwhy jztv lfrh'
+EMAIL_HOST_USER =''
+EMAIL_HOST_PASSWORD =''
 
